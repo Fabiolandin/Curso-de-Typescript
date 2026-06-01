55d6304ceef8063db406398988ee876a3a883fb9
Fabio Landin <flandin1990@gmail.com>
2026-06-01 12:06:00 -0300

    aula sobre tipo object

---
+++ src/Módulo 2/A0004-tipoobject.ts
@@ -0,0 +1,9 @@
+const objetoA = {
+    chaveA: 'Valor A',
+    chaveB: 'Valor B',
+    chaveC: 'Valor C',
+};
+
+//mudando o valor da chave a pra outra string
+objetoA.chaveA = 'Outro valor';
+//objetoA.chaveD = 'Valor D'; //isso vai dar erro pois a chave D não existe no objetoA
