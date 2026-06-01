1c87d94f6deeccc1cf6269713e8ae44bce16119f
Fabio Landin <flandin1990@gmail.com>
2026-06-01 12:07:34 -0300

    aula sobre tipo tuple

---
+++ src/Módulo 2/A0006-tipotuple.ts
@@ -0,0 +1,6 @@
+//Tuple é um array com tipos fixos
+const dadosCliente: [number, string, string] = [1, 'Fabio', 'Linhares'];
+
+dadosCliente[0] = 100;
+dadosCliente[1] = 'João';
+console.log(dadosCliente);
