ed66dcffa5e5e82a0fd3d9d48476b14728442b7a
Fabio Landin <flandin1990@gmail.com>
2026-06-09 10:08:49 -0300

    aula sobre this como tipo e polimorfico

---
+++ src/Módulo 3 - Tipos Avançados/A004-this-polimorfico.ts
@@ -0,0 +1,37 @@
+//This pode ser usado como um tipo
+export class Calculadora{
+    constructor(
+        public numero: number,
+    ){}
+
+    add(n: number): this {
+        this.numero += n;
+        return this;
+    }
+
+    sub(n: number): this {
+        this.numero -= n;
+        return this;
+    }
+
+    div(n: number): this {
+        this.numero /= n;
+        return this;
+    }
+
+    mul(n: number): this {
+        this.numero *= n;
+        return this;
+    }
+}
+
+export class SubCalculadora extends Calculadora {
+    pow(n: number): this {
+        this.numero **= n;
+        return this;
+    }
+}
+
+const calculadora = new SubCalculadora(10)
+calculadora.add(5).mul(2).div(2).sub(5).pow(2)
+console.log(calculadora)
