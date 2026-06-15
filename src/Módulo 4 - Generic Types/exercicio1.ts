c9595b0356458441e3624fd44114596852521d0f
Fabio Landin <flandin1990@gmail.com>
2026-06-15 16:19:36 -0300

    exercicio minha construções

---
+++ src/Módulo 4 - Generic Types/exercicio1.ts
@@ -0,0 +1,45 @@
+// Seu conceito de "linguagem" = VotationOption do professor
+export class linguagem{  // 👈 Isso é tipo VotationOption
+    constructor(
+        public nome: string,
+        public votos: number
+    ) {}
+}
+
+export class votacoes{
+    private opcoes: linguagem[] = []
+    
+    constructor(
+        public titulo: string
+    ) {
+        console.log(titulo)
+    }
+    
+    addLinguagem(linguagem: linguagem): void {
+        this.opcoes.push(linguagem);
+    }
+    
+    votar(indice: number): void {
+        if(!this.opcoes[indice]) return;
+        this.opcoes[indice].votos += 1;
+    }
+
+    exibirInfos(): void{
+        for(const opcao of this.opcoes){
+            console.log(opcao.nome + ' Votos: ' + opcao.votos)
+        }
+    }
+}
+
+const JavaScript = new linguagem('JavaScript', 0)
+const Python = new linguagem('Python', 0)
+const TypeScript = new linguagem('TypeScript', 0)
+const votacao = new votacoes('Votação de linguagens')
+votacao.addLinguagem(JavaScript)
+votacao.addLinguagem(TypeScript)
+votacao.addLinguagem(Python)
+votacao.votar(0)
+votacao.votar(1)
+votacao.votar(1)
+votacao.votar(2)
+votacao.exibirInfos()
